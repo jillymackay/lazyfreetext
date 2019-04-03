7bad54efe7db5f43da1bb519fb67d1d8b75e023f
MACKAY Jill <jmackay4@ed.ac.uk>
2019-04-03 13:11:45 +0100

    tidying up

--- R/buildspace.r
+++ R/buildspace.r
@@ -1,35 +1,35 @@
-# This is just a file for all my testing/building
-
-
-# Free text with one group (ID)
-ft1 <- readxl::read_excel("data/testdata.xlsx",
-                  sheet = "ft1")
-
-# Free text with one ID and one grouping factor
-ft2<-readxl::read_excel("data/testdata.xlsx",
-                        sheet = "ft2")
-
-# Free text with one ID and two grouping factors
-ft3 <- readxl::read_excel("data/testdata.xlsx",
-                 sheet = "ft3")
-
-
-extrastops <- tibble(lexicon = c("mine", "mine"), word = c("chapter", "chief"))
-extrastops <- rbind(stop_words, extrastops)
-
-
-t1 <- lazyfreetext(ft1, Text)
-
-quicktfplot(t1, lemma, ID, 3)
-
-
-
-t2 <- lazyfreetext(ft2, Text, stops = extrastops)
-
-quicktfplot(t2, lemma, Grp1, 5)
-
-
-
-t3 <- lazyfreetext(ft3, Text, stops = extrastops)
-
-quicktfplot(t3, lemma, Grp2, 5)
+# # This is just a file for all my testing/building
+# library(tidytext)
+#
+# # Free text with one group (ID)
+# ft1 <- readxl::read_excel("data/testdata.xlsx",
+#                   sheet = "ft1")
+#
+# # Free text with one ID and one grouping factor
+# ft2<-readxl::read_excel("data/testdata.xlsx",
+#                         sheet = "ft2")
+#
+# # Free text with one ID and two grouping factors
+# ft3 <- readxl::read_excel("data/testdata.xlsx",
+#                  sheet = "ft3")
+#
+#
+# extrastops <- dplyr::tibble(lexicon = c("mine", "mine"), word = c("chapter", "chief"))
+# extrastops <- rbind(stop_words, extrastops)
+#
+#
+# t1 <- lazyfreetext(ft1, Text)
+#
+# quicktfplot(t1, lemma, ID, 3)
+#
+#
+#
+# t2 <- lazyfreetext(ft2, Text, stops = extrastops)
+#
+# quicktfplot(t2, lemma, Grp1, 5)
+#
+#
+#
+# t3 <- lazyfreetext(ft3, Text, stops = extrastops)
+#
+# quicktfplot(t3, lemma, Grp2, 5)
